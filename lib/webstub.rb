046db7f7a336766690f875c54d2585bd2aeb11b7
Georges Auberger <3819+georges@users.noreply.github.com>
2024-08-10 08:25:39 -1000

    Defer include

--- lib/webstub.rb
+++ lib/webstub.rb
@@ -2,7 +2,8 @@ gem_name = File.basename(__FILE__).gsub(/\.rb$/, "")
 
 Motion::Project::App.setup do |app|
   app.development do
-    gem_files = Dir.glob(File.join(File.dirname(__FILE__), gem_name, "**/*.rb"))
-    app.files = gem_files + app.files
+    # Defer this so we don't include the code in release mode
+#    gem_files = Dir.glob(File.join(File.dirname(__FILE__), gem_name, "**/*.rb"))
+#    app.files = gem_files + app.files
   end
 end
